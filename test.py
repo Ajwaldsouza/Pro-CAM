58ee1d0557f81e345cad5773635470f74f53c937
Ajwaldsouza <ajwaldsouza@gmail.com>
2024-10-31 19:21:28 -0400

    NA

--- test.py
+++ test.py
@@ -15,8 +15,9 @@ class ImageCaptureApp(QWidget):
 
         # Camera setup
         self.camera = picamera2.Picamera2()
-        self.config = self.camera.configure(picamera2.PreviewConfig(), picamera2.CaptureConfig())
-        self.camera.start_preview(self.config['preview'])
+        self.config = self.camera.create_preview_configuration()
+        self.camera.configure(self.config)
+        self.camera.start()
 
         # GUI elements
         self.label = QLabel(self)
@@ -34,30 +35,8 @@ class ImageCaptureApp(QWidget):
         self.show()
 
     def capture_and_save(self):
-        label_text = self.label_input.text()
-        if not label_text:
-            print("Please enter a label")
-            return
-
-        # Capture image
-        request = self.camera.capture_request()
-        self.camera.capture(request)
-        image = self.camera.capture_buffer(request)['main']
-        qimage = QImage(image, 320, 240, QImage.Format_RGB888)
-        pixmap = QPixmap.fromImage(qimage)
-        self.label.setPixmap(pixmap)
-
-        # Add label to image (implement your desired labeling method)
-        # For example, using OpenCV or PIL:
-        # import cv2
-        # ... (convert QImage to OpenCV image)
-        # cv2.putText(image, label_text, (10, 230), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
-        # Save image
-        file_path, _ = QFileDialog.getSaveFileName(self, 'Save Image', '', 'JPEG (*.jpg)')
-        if file_path:
-            # Save the image using OpenCV or PIL
-            cv2.imwrite(file_path, image)
+        # Capture and save logic here
+        pass
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
